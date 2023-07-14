9dc57e493baec76e60c1cc93345f110b56cfe440
moogleKupo <tmaine@gmx.com>
2023-07-14 20:21:05 +0000

    set up user input

--- play.js
+++ play.js
@@ -3,7 +3,7 @@ const { connect: clientConnect } = require("./client.js");
 console.log("Connecting ...");
 clientConnect();
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 function connectServer() {
   const net = require("net");
   const conn = net.createConnection({
@@ -11,7 +11,7 @@ function connectServer() {
     port: 50541,
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
 
   // Handle incoming data
@@ -25,6 +25,24 @@ function connectServer() {
 
   return conn;
 }
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  // Handle other key inputs here if needed
+};
+
+const input = setupInput();
+input.on("data", handleUserInput);
 
 console.log("Connecting ...");
 connectServer();
